e39a764d06efeb7acacac03afa6da228bf0bb855
Lucas Gonzalez <lucasbertaudgonzalez@gmail.com>
2020-02-14 16:20:28 -0500

    created a database schema and seeds files in db folder, however, they will not run at the moment, created mock db in mysql workbench. Began to set up connection.js

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+id INT PRIMARY KEY AUTO 
+burger_name VARCHAR(30),
+devoured BOOLEAN
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Vegan Burger", false), ("Chicken Burger", true), ("Beast Burger", true);
