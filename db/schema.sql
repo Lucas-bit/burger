40f9ef1b6cccfd80630b6ed8254f65654d55dae8
Lucas Gonzalez <lucasbertaudgonzalez@gmail.com>
2020-02-17 22:31:12 -0500

    completed burgers_controller.js and updated my orm file as well

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,14 @@
+DROP DATABASE IF EXISTS burgers_db; 
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-id INT PRIMARY KEY AUTO 
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 burger_name VARCHAR(30),
 devoured BOOLEAN
 );
+
+
+
+
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,5 @@
+USE burgers_db;
 INSERT INTO burgers(burger_name, devoured)
 VALUES ("Vegan Burger", false), ("Chicken Burger", true), ("Beast Burger", true);
+
+SELECT * FROM burgers
